c67f225cd45b9ab4e456ac0adb4c6897f98ac93a
penhele <stephenhelk@gmail.com>
2025-08-01 16:06:02 +0700

    refactor: change e-commerce app to aug 2025

--- lib/model/project_list.dart
+++ lib/model/project_list.dart
@@ -130,7 +130,7 @@ final List<ProjectModel> projectList = [
     description:
         'Sebuah aplikasi e-commerce yang dibangun dengan visual yang menarik. Aplikasi menggunakan warna orange sebagai warna dasar. Saya membangun aplikasi ini dengan tutorial di youtube dan saya mengembangkan lebih lanjut mengenai aplikasi ini. Sumber saya cantumkan di youtube bawah ini.',
     thumbnail: SImages.ecommerceApp,
-    duration: 'Jul — Present',
+    duration: 'Jul — Aug 2025',
     githubUrl: 'https://github.com/penhele/ecommerce-app',
     youtubeUrl: 'https://youtu.be/rX9kvw79kgk',
     language: [
